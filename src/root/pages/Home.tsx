20efd9f5be796ac9e6bd3103f7c546989b4b0ef3
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-10-21 23:45:09 +0600

    ....

--- src/root/pages/Home.tsx
+++ src/root/pages/Home.tsx
@@ -1,6 +1,8 @@
-import NavBar from "@/components/shared/NavBar"
 import FinisherHeaderComponent from "@/components/style/FinisherHeaderComponent"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { useEffect, useState } from "react";
+import MyImage from "../../assets/images/mushfik.png";
+import { Facebook, Github, Linkedin } from "lucide-react";
 
 function Home() {
     const arr = ['Build', 'design', 'redesign', 'optimize', 'systemize'];
@@ -14,6 +16,7 @@ function Home() {
     return (
         <div className="w-full center flex-col">
             <FinisherHeaderComponent />
+            {/* hero section */}
             <section className="hero w-full h-[500px] md:h-[550px] center flex-col gap-4">
                 <h1 className="text-[28px] md:text-4xl lg:text-6xl capitalize text-center font-sans">Transforming Your Vision
                     <br></br> into a
@@ -35,9 +38,99 @@ function Home() {
                 </p>
             </section>
 
-            <section className="h-screen w-full center">
-                <h1>hello</h1>
+            {/* about section */}
+            <section className="h-full w-full center justify-start flex-col text-text">
+                <h2 className="text-4xl font-bold">Who We Are</h2>
+                <p className="px-6 md:px-24 py-8">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, hic dignissimos unde voluptas eos laborum suscipit! Neque, possimus rem! Adipisci possimus tenetur error dicta dolorum officiis distinctio deleniti natus maiores!
+                    <br></br><br></br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, hic dignissimos unde voluptas eos laborum suscipit! Neque, possimus rem! Adipisci possimus tenetur error dicta dolorum officiis distinctio deleniti natus maiores!
+                </p>
+                <div className="center flex-col">
+
+                    <h2 className="uppercase text-xl font-medium pt-4 pb-12">teams members</h2>
+
+                    <div className="w-full center gap-12 flex-wrap">
+                        {/* card 01 */}
+                        <div className="card w-[240px] h-[280px] glass-card center rounded-[15px] transition-transform duration-300 group cursor-pointer">
+                            <div className="w-[95%] h-[95%] bg-gray-100 rounded-[12px] overflow-hidden">
+                                {/* <img src="../../assets/images/mushfik.png" alt="mushfik" /> */}
+                                <img src={MyImage} className="w-full h-full object-cover object-top" alt="" />
+                                <div className="w-full bg-gray-100 center flex-col gap-4 transition-transform group-hover:-translate-y-[100%] py-4">
+                                    <h2 className="text-lg font-bold">Md. Mushfiqur Rahman</h2>
+                                    <p className="text-md font-medium">Fornt-End Developer</p>
+                                    <ul className="center gap-6">
+                                        <li><Facebook strokeWidth={1} className="hover:text-primary" /></li>
+                                        <li><Linkedin strokeWidth={1} className="hover:text-primary" /></li>
+                                        <li><Github strokeWidth={1} className="hover:text-primary" /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        {/* card 02 */}
+                        <div className="card w-[240px] h-[280px] glass-card center rounded-[15px] transition-transform duration-300 group cursor-pointer">
+                            <div className="w-[95%] h-[95%] bg-gray-100 rounded-[12px] overflow-hidden">
+                                {/* <img src="../../assets/images/mushfik.png" alt="mushfik" /> */}
+                                <img src={MyImage} className="w-full h-full object-cover object-top" alt="" />
+                                <div className="w-full bg-gray-100 center flex-col gap-4 transition-transform group-hover:-translate-y-[100%] py-4">
+                                    <h2 className="text-lg font-bold">Md. Mushfiqur Rahman</h2>
+                                    <p className="text-md font-medium">Fornt-End Developer</p>
+                                    <ul className="center gap-6">
+                                        <li><Facebook strokeWidth={1} className="hover:text-primary" /></li>
+                                        <li><Linkedin strokeWidth={1} className="hover:text-primary" /></li>
+                                        <li><Github strokeWidth={1} className="hover:text-primary" /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        {/* card-03 */}
+                        <div className="card w-[240px] h-[280px] glass-card center rounded-[15px] transition-transform duration-300 group cursor-pointer">
+                            <div className="w-[95%] h-[95%] bg-gray-100 rounded-[12px] overflow-hidden">
+                                {/* <img src="../../assets/images/mushfik.png" alt="mushfik" /> */}
+                                <img src={MyImage} className="w-full h-full object-cover object-top" alt="" />
+                                <div className="w-full bg-gray-100 center flex-col gap-4 transition-transform group-hover:-translate-y-[100%] py-4">
+                                    <h2 className="text-lg font-bold">Md. Mushfiqur Rahman</h2>
+                                    <p className="text-md font-medium">Fornt-End Developer</p>
+                                    <ul className="center gap-6">
+                                        <li><Facebook strokeWidth={1} className="hover:text-primary" /></li>
+                                        <li><Linkedin strokeWidth={1} className="hover:text-primary" /></li>
+                                        <li><Github strokeWidth={1} className="hover:text-primary" /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
+
+            {/* showcase section */}
+            <section className="h-screen w-full bg-gray-100 center flex-col">
+
+                <div className="center">
+                    <Carousel className="w-full bg-green-100 rounded-xl p-4">
+                        <CarouselContent className="-ml-2">
+                            {Array.from({ length: 5 }).map((_, index) => (
+                                <CarouselItem
+                                    key={index}
+                                    className="basis-full sm:basis-1/2 lg:basis-1/3 px-2"
+                                >
+                                    <div className="rounded-lg overflow-hidden border-2 border-gray-200 shadow-md hover:shadow-lg transition-shadow duration-300">
+                                        <img
+                                            src={MyImage}
+                                            alt={`Slide ${index + 1}`}
+                                            className="w-full h-full object-cover object-center"
+                                        />
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+
+                        <CarouselPrevious className="cursor-pointer left-2" />
+                        <CarouselNext className="cursor-pointer right-2" />
+                    </Carousel>
+                </div>
+            </section>
+
         </div>
     )
 }
